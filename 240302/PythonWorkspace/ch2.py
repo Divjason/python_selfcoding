0a266045311b751da424fa938142f7289cd530b6
Divjason <126494515+Divjason@users.noreply.github.com>
2024-03-02 22:04:37 +0900

    Add files via upload

---
+++ 240302/PythonWorkspace/ch2.py
@@ -0,0 +1,75 @@
+# print(5)
+# print(-10)
+# print(3.14)
+# print(1000)
+
+# print(5 + 3)
+# print(2 * 8)
+# print(6 / 3)
+# print(3 * (3 + 1))
+
+# print("풍선")
+# print("나비")
+# print("abcdefg")
+# print("10")
+# print("파이썬" * 3)
+
+# print(5 >10)
+# print(5 <10)
+
+# print(True)
+# print(False)
+
+# print(not(5 > 10))
+
+# name = "연탄이"
+# animal = "개"
+# age = 4
+# hobby = "산책"
+
+# print("반려동물을 소개해 주세요!")
+# print("우리 집 반려동물은 " + animal + "인데, 이름이 " + name + "에요.")
+# print(name + "는 " + str(age) + "살이고, " + hobby + "을 아주 좋아해요.")
+# print("연탄이는 수컷인가요?")
+# print("네.")
+
+# name = "해피"
+# animal = "고양이"
+# age = 4
+# hobby = "낮잠"
+
+# print("반려동물을 소개해 주세요!")
+# print("우리 집 반려동물은 " + animal + "인데, 이름이 " + name + "에요.")
+# print(name + "는 " + str(age) + "살이고, " + hobby + "을 아주 좋아해요.")
+# print("연탄이는 수컷인가요?")
+# print("네.")
+
+# print(int("3") + "입니다.")
+# print(int(3.5))
+# print(int("삼"))
+
+# print(float("3.5"))
+# print(float(3))
+
+# print(str(3) + "입니다.")
+# print(str(3.5) + "입니다.")
+
+"""
+print(type(3))
+print(type("3"))
+print(type(3.5))
+print(type(str(3)))
+"""
+
+# animal = "개"
+# age = 4
+# hobby = "산책"
+
+# print("반려동물을 소개해 주세요.")
+# name = "연탄이"
+# print("우리 집 반려동물은 " + animal + "인데, 이름이 " + name + "에요")
+# hobby = "수영"
+# print(name + "는 " + str(age) + "살이고, " + hobby + "을 아주 좋아해요.")
+
+station = "사당"
+print(station + "행 열차가 들어오고 있습니다.")
